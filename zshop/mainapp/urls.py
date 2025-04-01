4577b36f77693f3948ebb41576726e21be57c241
archith <archithkumarkodipalli07@gamil.com>
2025-04-01 09:05:58 -0700

    added images and crud operations

--- zshop/mainapp/urls.py
+++ zshop/mainapp/urls.py
@@ -5,4 +5,13 @@ from . import views
 urlpatterns = [
 
     path('', views.homeView, name='homepage'),
+    # path('', views.aboutView, name = 'aboutpage'),
+    # path('', views.contactView, name= 'contactpage'),
+    path('product/<int:pk>', views.ProductDetails.as_view(), name = 'prod_details'),
+    path('products/add', views.AddProduct.as_view(), name='add_product'),
+    path('products/edit/<int:pk>', views.EditProduct.as_view() ,name='edit_product'),
+    path('products/del/<int:pk>', views.DelProduct.as_view() ,name='del_product')
+
 ]
+
+
--- zshop/mainapp/views.py
+++ zshop/mainapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django. template import loader
 from django.http import HttpResponse
 
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
 from .models import Product
 
 def homeView(request):
@@ -10,6 +12,57 @@ def homeView(request):
     template = loader.get_template('home.html')
 
     context = {
-
+        # context data to be pulled from the db
+        'products' : Product.objects.all()
+        # the above line of code is equivalent to SELECT * FROM product_table;
     }
     return HttpResponse(template.render(context, request))
+class ProductDetails(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+
+def aboutView(request):
+    template = loader.get_template('about.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def contactView(request):
+    template = loader.get_template('contact.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+
+#CRUD Opeartions
+
+
+
+
+class AddProduct(CreateView):
+    model = Product
+    template_name = 'add_product.html'
+    fields = '__all__'
+    success_url = '/'
+
+
+class EditProduct(UpdateView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'edit_product.html'
+    fields = ['img','price','stock']
+    success_url = '/'
+
+
+# D -Delete
+
+class DelProduct(DeleteView):
+    model = Product
+    template_name = 'del_product.html'
+    success_url = '/'
+        
+        
+        
+   
+
+
+
